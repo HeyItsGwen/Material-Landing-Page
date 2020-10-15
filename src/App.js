574722c3a2abd199a2fa61b918d4f74ea585c819
Gwen <itsgwenhey@gmail.com>
2020-10-14 19:21:04 -0500

    nav styling
    added styling and responsiveness to nav bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header class='d-flex'>
-        <img src='' alt='company logo'/>
-        <nav className="App-header" id='navBar'>
+      <header className='App-header' class='d-flex'>
+        <nav class='d-flex flex-column flex-md-row justify-content-between align-items-end' id='navBar'>
           <NavLinks />
+          <SocialLinks />
         </nav>
       </header>
     </div>
@@ -19,7 +19,7 @@ function NavLinks() {
   return (
     <ul class='d-flex'>
       <li>
-        <a href='#navBar'>To Top</a>
+        <a href='#navBar'><i class="fas fa-arrow-alt-circle-up"></i></a>
       </li>
       <li>
         <a href='#about'>About</a>
@@ -27,6 +27,16 @@ function NavLinks() {
       <li>
         <a href='#retailers'>Find a Retailer</a>
       </li>
+    </ul>
+  )
+}
+
+function SocialLinks() {
+  return (
+    <ul class='d-flex' id='socialLinks'>
+      <li><a href='#' target='blank'><i class="fab fa-facebook"></i></a></li>
+      <li><a href='#' target='blank'><i class="fab fa-twitter"></i></a></li>
+      <li><a href='#' target='blank'><i class="fab fa-instagram"></i></a></li>
     </ul>
   )
 }
