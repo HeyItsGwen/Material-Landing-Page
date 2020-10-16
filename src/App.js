a45af18253a0a300e2a7beb71fcf9c0c79da7c5c
Gwen <itsgwenhey@gmail.com>
2020-10-16 18:14:57 -0500

    more styling for about, retail, and contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import watch from './images/watch.jpg';
 
 function App() {
   return (
@@ -12,6 +11,9 @@ function App() {
       </header>
       <main>
         <HeroSection />
+        <AboutSection />
+        <FindARetailer />
+        <ContactSection />
       </main>
     </div>
   );
@@ -21,15 +23,18 @@ export default App;
 
 function NavLinks() {
   return (
-    <ul class='d-flex'>
+    <ul class='d-flex navLinks'>
       <li class='firstLi'>
-        <a href='#header'><i class="fas fa-arrow-alt-circle-up"></i></a>
+        <a href='#header'><i class="fas fa-igloo"></i></a>
       </li>
       <li>
-        <a href='#about'>About</a>
+        <a href='#aboutSection'>About</a>
       </li>
       <li>
         <a href='#retailers'>Find a Retailer</a>
+      </li>
+      <li>
+        <a href='#contact'>Contact</a>
       </li>
     </ul>
   )
@@ -47,11 +52,104 @@ function SocialLinks() {
 
 function HeroSection() {
   return (
-    <section class='jumbotron d-flex flex-column flex-md-row justify-content-between align-items-center'>
+    <section class='jumbotron d-flex flex-md-row justify-content-center justify-content-md-between align-items-center'>
       <span class='align-self-md-center' id='titleP'>
         <h1>It's<br/>Your<br/>Brand</h1>
-        <p>Put it in the spotlight; Show it off to the world</p>
+        <p>Show it off to the world</p>
       </span>
     </section>
+  )
+}
+
+function AboutSection() {
+  return (
+    <section id='aboutSection' class='d-flex flex-column align-items-center'>
+      <div class='row'>
+        <div class='col-12 col-sm-6 col-md-4 text-center d-flex aboutInfo justify-content-center'>
+          <span class='d-block'>
+            <i class="fas fa-info-circle"></i>
+            <p>Show people how good your brand is, let them know how much they need you</p>
+          </span>
+        </div>
+        <div class='col-12 col-sm-6 col-md-4 text-center d-flex aboutInfo justify-content-center'>
+          <span class='d-block alternateGrey'>
+            <i class="far fa-paper-plane"></i>
+            <p>Maybe talk about your successes, your achievements, or plans...</p>
+          </span>
+        </div>
+        <div class='col-12 d-none d-md-block col-md-4 col-lg-4 text-center aboutInfo'>
+          <span class='d-block'>
+            <i class="fas fa-pager"></i>
+            <p>Or let them know how good your customer service is, or the reviews you've gotten.</p>
+          </span>
+        </div>
+      </div>
+      <div class='row'>
+        <div class='col-12 col-sm-6 col-md-4 text-center d-flex aboutInfo justify-content-center'>
+          <span class='d-block alternateGrey'>
+            <i class="fas fa-scroll"></i>
+            <p>It could be your employees, or customer service team, with their headshots</p>
+          </span>
+        </div>
+        <div class='col-12 col-sm-6 col-md-4 text-center d-flex aboutInfo justify-content-center'>
+          <span class='d-block'>
+            <i class="fas fa-palette"></i>
+            <p>Let the world know what you have to offer in short, bite-sized portions</p>
+          </span>
+        </div>
+        <div class='col-12 d-none d-md-block col-md-4 col-lg-4 text-center aboutInfo'>
+          <span class='d-block alternateGrey'>
+            <i class="fas fa-arrow-circle-down"></i>
+            <p>Then send them further to something actionable: a shop, social media, a contact form...</p>
+          </span>
+        </div>
+      </div>
+      <div class='row'>
+        <div class='col-12 downArrow'>
+          <a href='#retailers'><i class="fas fa-chevron-down"></i></a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+function FindARetailer() {
+  return (
+    <section id='retailers' class='d-flex flex-column align-items-center'>
+      <h3>Find A Retailer</h3>
+      <div class='row' id='retailerDiv'>
+        <div class='col-6'>
+          <p>ONE</p>
+          <p>SHOP LINKS</p>
+        </div>
+        <div class='col-6'>
+          <p>TWO</p>
+          <p>Physical Locations</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+function ContactSection() {
+  return (
+    <div id='contact' class='d-flex flex-column align-items-center'>
+      <h3>Contact Us</h3>
+      <div class='row justify-content-around' id='contactDiv'>
+        <div class='col-sm-6 col-12 d-flex flex-column justify-content-center contactSections'>
+          <p class='text-center'>Find us on social media</p>
+          <ul class='d-flex justify-content-around bottomSocials'>
+            <li><a href='http://www.facebook.com' target='blank'><i class="fab fa-facebook"></i></a></li>
+            <li><a href='http://www.twitter.com' target='blank'><i class="fab fa-twitter"></i></a></li>
+            <li><a href='http://www.instagram.com' target='blank'><i class="fab fa-instagram"></i></a></li>
+          </ul>
+          <p class='text-center'>Or send us an email at:</p>
+          <p class='text-center'><a class='text-center' href='emailto:exampleemail@email.com'>YourBrand@email.com</a></p>
+        </div>
+        <div class='col-sm-6 col-12 contactSections'>
+          <p>contact form</p>
+        </div>
+      </div>
+    </div>
   )
 }
