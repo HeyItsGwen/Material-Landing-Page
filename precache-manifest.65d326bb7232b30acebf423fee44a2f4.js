7fc7cff843ecda8d2c3081943f3d8b556b9c51fc
Gwen <itsgwenhey@gmail.com>
2020-10-21 05:23:31 -0500

    Updates

---
+++ precache-manifest.65d326bb7232b30acebf423fee44a2f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6639cb57bee8a1b9c47d4418b85cb5ef",
+    "url": "/Simple-Landing-Page/index.html"
+  },
+  {
+    "revision": "4ae308a8dc547fb71f74",
+    "url": "/Simple-Landing-Page/static/css/main.797f82a7.chunk.css"
+  },
+  {
+    "revision": "49ae16cbc3ca2e1ee36f",
+    "url": "/Simple-Landing-Page/static/js/2.cc28adf8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Simple-Landing-Page/static/js/2.cc28adf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae308a8dc547fb71f74",
+    "url": "/Simple-Landing-Page/static/js/main.6bb0d399.chunk.js"
+  },
+  {
+    "revision": "b9bd6fbc9b5581b0c6c1",
+    "url": "/Simple-Landing-Page/static/js/runtime-main.20b679ec.js"
+  },
+  {
+    "revision": "243d94750cdffbfad13cfeea8b41d3ac",
+    "url": "/Simple-Landing-Page/static/media/bigcamera.243d9475.jpg"
+  },
+  {
+    "revision": "efc76e0fc0824d1a5573f37c7bea8a9e",
+    "url": "/Simple-Landing-Page/static/media/cameratransparent.efc76e0f.png"
+  },
+  {
+    "revision": "14579831272eb8c9cb5ce9acc6f3a276",
+    "url": "/Simple-Landing-Page/static/media/map.14579831.jpg"
+  }
+]);
